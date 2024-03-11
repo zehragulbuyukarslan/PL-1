ac0c2416cd7859c46b4665f8bfd34cbf280a072f
Zehra Gul Buyukarslan <151182584+zehragulbuyukarslan@users.noreply.github.com>
2024-03-11 17:38:43 +0300

    Add files via upload

---
+++ homework questions/C codes/01- girilen iki sayıyı toplayıp yazdıran kod.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	int a,b,c;
+	printf("ilk sayiyi giriniz:\n");
+	scanf("%d", &a);
+	printf("ikinci sayiyi giriniz:\n");
+	scanf("%d", &b);
+	c=a+b;
+	printf("sayilarin toplami: %d" , c );
+	
+return 0;
+}
