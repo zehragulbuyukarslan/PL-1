555f590a3be1b094889714a76dc3357790a1eb28
Zehra Gul Buyukarslan <151182584+zehragulbuyukarslan@users.noreply.github.com>
2024-03-11 17:47:45 +0300

    Add files via upload

---
+++ homework questions/C codes/29- find the sum of the first 50 natural numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i,sum;
+	sum=0;
+	for(i=1; i<=50; i++)
+	sum=sum+i;
+	printf("\n\nsum of the first 50 natural numbers: %d\n\n\n",sum);
+}
---
+++ homework questions/C codes/30- calculate 2^4.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\n\n2 power 4 is equal: %d\n\n\n",2*2*2*2);
+}
